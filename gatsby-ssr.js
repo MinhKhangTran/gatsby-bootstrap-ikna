60027f8abb3c34896ea5ac822be5fd0beb68dd9b
MinhKhangTran <67480568+MinhKhangTran@users.noreply.github.com>
2021-10-08 14:52:16 +0200

    :sparkles: add bs to head

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,9 @@
 const React = require("react")
 
 
-
+const HeadComponents = [
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+]
 
 const BodyAttributes = {
     "data-bs-spy":"scroll",
@@ -14,8 +16,9 @@ const BodyAttributes = {
 }
 
 exports.onRenderBody = ({
+  setHeadComponents,
   setBodyAttributes
 }, pluginOptions) => {
- 
+  setHeadComponents(HeadComponents)
   setBodyAttributes(BodyAttributes)
 }
