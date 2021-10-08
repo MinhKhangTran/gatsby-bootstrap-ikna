2edae5549ee19fcf98c6dbb4db1f42fd9cdf65df
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-08 06:22:30 +0200

    :sparkles: navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,28 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import logo from "../images/LOGO.svg";
 
 const Navigation = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const onScroll = () => {
+      window.scrollY === 0 ? setScrolled(false) : setScrolled(true);
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, [scrolled]);
+
   return (
-    <Navbar id="mainNav" fixed="top" bg="white" expand="lg">
+    <Navbar
+      id="mainNav"
+      fixed="top"
+      bg="white"
+      expand="lg"
+      className={`${scrolled && "nav_scrolled"}`}
+    >
       <Container>
         <Link to="/" className="navbar-brand">
           <img src={logo} alt="ikna" />
