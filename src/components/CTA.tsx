37cdf031a535a609c10246d9fffb103590ea1298
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-08 05:36:35 +0200

    :sparkles: add CTA

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-import { Container, Button } from "react-bootstrap";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import React, { useEffect, useState } from "react";
+import { Container, Button, Row, Col } from "react-bootstrap";
+
+interface IUsers {
+  results: IPhotos[];
+}
+interface IPhotos {
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+}
 
 const CTA = () => {
+  // Fetch data from randomuser api
+  const [users, setUsers] = useState<IPhotos[]>([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      const data: IUsers = await (
+        await fetch("https://randomuser.me/api/?results=9&inc=picture")
+      ).json();
+      setUsers(data.results);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <section id="cta" className="bg-orange-100">
       <Container>
@@ -9,7 +33,20 @@ const CTA = () => {
           Join thousands of other{" "}
           <span className="bg-orange-400 px-1">satisfied</span> students
         </h2>
-        <Button size="lg">Join Now</Button>
+        <div className="users_wrapper mt-5">
+          {users.map((user, index) => {
+            return (
+              <div className="user">
+                <img src={user.picture.thumbnail} alt={`user ${index}`} />
+              </div>
+            );
+          })}
+        </div>
+        <div className="button_wrapper d-flex justify-content-center mt-4">
+          <Button href="#" size="lg" className="text-white fw-bold mx-auto">
+            Join Now
+          </Button>
+        </div>
       </Container>
     </section>
   );
